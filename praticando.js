7ef78fdce97d5baa6de2b3d767efcf909f81f0e1
Gustavo Castanho <65131471+duartecgustavo@users.noreply.github.com>
2021-06-06 14:03:52 -0300

    att

---
+++ praticando.js
@@ -0,0 +1,27 @@
+// Crie duas expressões que retornem NaN
+
+var exp1 = 'gustavo' - 10
+
+console.log(exp1)
+
+var value1 = 'gustavo'
+var value2 = 'amanda'
+
+var exp2 = value1 - value2
+
+console.log(exp2)
+
+var n1 = '200'
+var n2 = 50
+
+console.log(+n1+n2)
+
+var numero = 50
+console.log(++numero)
+
+var peso = +'100' / 2
+
+var unidade ='kg'
+
+var pesoFinal
+console.log(pesoFinal = peso+unidade)
